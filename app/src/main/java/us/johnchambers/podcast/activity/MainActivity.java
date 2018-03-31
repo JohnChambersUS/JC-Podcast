ede71716055dc2d8e5357f8637c51c58c082e0ae
John Chambers <jadecalyx@outlook.com>
2018-03-31 11:25:01 -0400

    added crashlytics to app

--- app/src/main/java/us/johnchambers/podcast/activity/MainActivity.java
+++ app/src/main/java/us/johnchambers/podcast/activity/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 import us.johnchambers.podcast.R;
 import us.johnchambers.podcast.misc.MyFileManager;
 
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         MyFileManager.getInstance(this).makeStorageDirectories();
