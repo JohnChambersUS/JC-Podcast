4ee51e125c1a4f4ecbb5dbb45044a9dd579c4032
John Chambers <jadecalyx@outlook.com>
2018-05-13 14:07:48 -0400

    drawer now opens on app opening

--- app/src/main/java/us/johnchambers/podcast/activity/MainNavigationActivity.java
+++ app/src/main/java/us/johnchambers/podcast/activity/MainNavigationActivity.java
@@ -14,6 +14,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -86,6 +87,9 @@ public class MainNavigationActivity extends AppCompatActivity
         EventBus.getDefault().register(this);
 
         initDatabaseIfNeeded();
+
+        //leave as last item
+        drawer.openDrawer(Gravity.LEFT);
     }
 
 
